0f1cfcb5a03a20d2ee2f714ef7fc359acc52251b
Marcin Tojek <tojek.m@gmail.com>
2023-04-15 13:09:28 +0200

    Debouncing and refactor

--- main.c
+++ main.c
@@ -5,26 +5,61 @@
 #include <string.h>
 #include <limits.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-const int buttonWiPins[] = {2, 5, 4, 6};
+#define DEBOUNCE_TIME 150
 
-const int wiPinsToGpio[] = {73, 70, 227, 75};
+int debounceTimes[] = {0, 0, 0, 0};
+
+bool debounceFunction(int buttonIndex) {
+ if (millis() < debounceTimes[buttonIndex] + DEBOUNCE_TIME) {
+    return true;
+  }
+  debounceTimes[buttonIndex] = millis();
+  return false;
+}
+
+#define BUTTON1_ID 0
+#define BUTTON2_ID 1
+#define BUTTON3_ID 2
+#define BUTTON4_ID 3
 
 void button1(void) {
+  if (debounceFunction(BUTTON1_ID)) {
+		return;
+  }
+
   printf("Button 1 pressed\n");
 }
 
 void button2(void) {
+  if (debounceFunction(BUTTON2_ID)) {
+    return;
+  }
+
   printf("Button 2 pressed\n");
 }
 
 void button3(void) {
+  if (debounceFunction(BUTTON3_ID)) {
+    return;
+  }
+
   printf("Button 3 pressed\n");
 }
 
 void button4(void) {
+  if (debounceFunction(BUTTON4_ID)) {
+    return;
+  }
+
   printf("Button 4 pressed\n");
 }
+
+void (*buttonFunctions[])(void) = { &button1, &button2, &button3, &button4 };
+
+const int buttonWiPins[] = {2, 5, 4, 6};
+const int wiPinsToGpio[] = {73, 70, 227, 75};
 
 void unexportButtonPins() {
 	printf("Unexport button pins...\n");
@@ -45,20 +80,19 @@ void unexportButtonPins() {
 	}
 }
 
+void configureIntterupt(int buttonIndex) {
+  int wiPin = buttonWiPins[buttonIndex];
+  printf("\tpin %d\n", wiPin);
+
+	wiringPiISR(wiPin, INT_EDGE_FALLING, buttonFunctions[buttonIndex]);
+}
+
 void configureInterrupts() {
   printf("Configure interrupts...\n");
 
-  printf("\tpin %d\n", buttonWiPins[0]);
-  wiringPiISR(buttonWiPins[0], INT_EDGE_FALLING, &button1);
-
-  printf("\tpin %d\n", buttonWiPins[1]);
-  wiringPiISR(buttonWiPins[1], INT_EDGE_FALLING, &button2);
-  
-  printf("\tpin %d\n", buttonWiPins[2]);
-  wiringPiISR(buttonWiPins[2], INT_EDGE_FALLING, &button3);
-  
-  printf("\tpin %d\n", buttonWiPins[3]);
-  wiringPiISR(buttonWiPins[3], INT_EDGE_FALLING, &button4);
+  for (int i = 0; i < sizeof(buttonWiPins) / sizeof(buttonWiPins[0]); i++) {
+    configureIntterupt(i);
+  }
 }
 
 int main() {
